3e08d1a9fee997b0a936d97e2d3ec57179cb14b2
Mihkel-Kritsmann <mihkelkritsmann@gmail.com>
2024-04-18 14:44:36 +0300

    Add project files.

---
+++ BlazorApp/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+        public DateTime TimeCreated { get; set; }
+        public DateTime Deadline { get; set; }
+       
+    }
+}
